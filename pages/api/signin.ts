f69909ab93b61dcd8cebb329538174972b3e9428
Loic Laudet <dlodz.me@gmail.com>
2022-03-28 09:27:19 +0200

    feat: add signin api

---
+++ pages/api/signin.ts
@@ -0,0 +1,43 @@
+import bcrypt from "bcrypt";
+import cookie from "cookie";
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+
+import prisma from "../../lib/prisma";
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (user && bcrypt.compareSync(password, user.password)) {
+    const token = jwt.sign(
+      {
+        email: user.email,
+        id: user.id,
+        time: Date.now(),
+      },
+      "hello",
+      { expiresIn: "8h" }
+    );
+
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("TRAX_ACCESS_TOKEN", token, {
+        httpOnly: true,
+        maxAge: 8 * 60 * 60,
+        path: "/",
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production",
+      })
+    );
+    res.json(user);
+  } else {
+    res.status(401);
+    res.json({ error: "Wrong email or password" });
+  }
+}
